99135bd99ab5eefb5fa175ce06434bc7ccda0350
nikolaiwarner <n@nikolaiwarner.com>
2011-03-27 23:25:18 -0400

    built a simple js pomodoro timer

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,61 @@
 var Aumodoro = {
 
+  timer: {
+    seconds_elapsed: 0,
+    count_up: true,
+    pom_length_in_minutes: 25,
+    display_element: undefined,
+    interval: undefined,
+    minutes: 0,
+    seconds: 0,
+    
+    
+    start: function() {
+      var self = this;
+      clearInterval(this.interval);
+      this.interval = setInterval(function(){
+        self.seconds_elapsed = self.seconds_elapsed + 1;
+        self.update_interface();
+        
+        if (self.seconds_elapsed >= (60*self.pom_length_in_minutes)) {
+          self.complete();
+        }
+      }, 1000);
+      
+      // ! auto update pom record start
+    },
+    
+    complete: function() {
+      clearInterval(this.interval);
+      this.seconds_elapsed = 0;
+      // ! auto update pom record end
+    },
+    
+    update_interface: function(){
+      if (this.count_up) {
+        this.minutes = parseInt(this.seconds_elapsed / 60, 10);
+        this.seconds = this.seconds_elapsed % 60.0;
+      } else {
+        this.minutes = (this.pom_length_in_minutes-1) - parseInt(this.seconds_elapsed / 60, 10);
+        this.seconds = 60 - this.seconds_elapsed % 60.0;
+      }
+      if (this.minutes < 10) {
+        this.minutes = "0"+this.minutes;
+      }
+      if (this.seconds < 10) {
+        this.seconds = "0"+this.seconds;
+      }
+      $(this.display_element).html(this.minutes+":"+this.seconds);
+    },
+    
+    init: function(options) {
+      this.pom_length_in_minutes = options.pom_length_in_minutes || this.pom_length_in_minutes;
+      this.count_up = options.count_up;
+      this.seconds_elapsed = options.seconds_elapsed || this.seconds_elapsed;
+      this.display_element = options.display_element || this.display_element;
+    }
+  }
+
   
 };
 
