062380accd189bcb07843f683785ae0345a58845
nikolaiwarner <n@nikolaiwarner.com>
2011-05-13 23:19:07 -0400

    stylin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,10 @@ module ApplicationHelper
     content_for(:title, string)
     string
   end 
+  
+  def back_link title, url
+    content_for :back_link, link_to(raw("&larr; #{title}"), url)
+  end
+  
 
 end
